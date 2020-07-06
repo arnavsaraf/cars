d14773be7e76a70099a6e704240fb247ae13c340
arnavsaraf <65659024+arnavsaraf@users.noreply.github.com>
2020-07-06 16:03:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var position;
+var gameState=0;
+var playerCount, database,form,game,player;
+var canvas;
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  canvas = createCanvas(400,400);
+
+
+function draw(){
+ 
+  
+}
